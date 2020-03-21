a0d803db387f76f7ec85d172a86b784550877811
Vincent Fartmann <v.fartmann@evorion.de>
2020-03-21 14:37:11 +0100

    NRF24 RX working

--- src/main.c
+++ src/main.c
@@ -70,6 +70,8 @@ uint8_t payload_length = 32;
 
 nRF24_TXResult tx_res;
 
+uint32_t i,j,k;
+
 int main(void) {
 
 	prvSetupHardware();
@@ -80,102 +82,74 @@ int main(void) {
 	rtc_current_time_unix = rtc_old_time_unix = rtc_get_unix_time(&sm_time,
 			&sm_date);
 
-	// This is simple transmitter with Enhanced ShockBurst (to one logic address):
-	//   - TX address: 'ESB'
+    // This is simple receiver with Enhanced ShockBurst:
+	//   - RX address: 'ESB'
 	//   - payload: 10 bytes
 	//   - RF channel: 40 (2440MHz)
 	//   - data rate: 2Mbps
 	//   - CRC scheme: 2 byte
 
-	// The transmitter sends a 10-byte packets to the address 'ESB' with Auto-ACK (ShockBurst enabled)
+    // The transmitter sends a 10-byte packets to the address 'ESB' with Auto-ACK (ShockBurst enabled)
 
-	// Set RF channel
-	nRF24_SetRFChannel(40);
+    // Set RF channel
+    nRF24_SetRFChannel(40);
 
-	// Set data rate
-	nRF24_SetDataRate(nRF24_DR_2Mbps);
+    // Set data rate
+    nRF24_SetDataRate(nRF24_DR_2Mbps);
 
-	// Set CRC scheme
-	nRF24_SetCRCScheme(nRF24_CRC_2byte);
+    // Set CRC scheme
+    nRF24_SetCRCScheme(nRF24_CRC_2byte);
 
-	// Set address width, its common for all pipes (RX and TX)
-	nRF24_SetAddrWidth(3);
+    // Set address width, its common for all pipes (RX and TX)
+    nRF24_SetAddrWidth(3);
 
-	// Configure TX PIPE
-	static const uint8_t nRF24_ADDR[] = { 'E', 'S', 'B' };
-	nRF24_SetAddr(nRF24_PIPETX, nRF24_ADDR); // program TX address
-	nRF24_SetAddr(nRF24_PIPE0, nRF24_ADDR); // program address for pipe#0, must be same as TX (for Auto-ACK)
+    // Configure RX PIPE
+    static const uint8_t nRF24_ADDR[] = { 'E', 'S', 'B' };
+    nRF24_SetAddr(nRF24_PIPE1, nRF24_ADDR); // program address for pipe
+    nRF24_SetRXPipe(nRF24_PIPE1, nRF24_AA_ON, 10); // Auto-ACK: enabled, payload length: 10 bytes
 
-	// Set TX power (maximum)
-	nRF24_SetTXPower(nRF24_TXPWR_0dBm);
+    // Set TX power for Auto-ACK (maximum, to ensure that transmitter will hear ACK reply)
+    nRF24_SetTXPower(nRF24_TXPWR_0dBm);
 
-	// Configure auto retransmit: 10 retransmissions with pause of 2500s in between
-	nRF24_SetAutoRetr(nRF24_ARD_2500us, 10);
+    // Set operational mode (PRX == receiver)
+    nRF24_SetOperationalMode(nRF24_MODE_RX);
 
-	// Enable Auto-ACK for pipe#0 (for ACK packets)
-	nRF24_EnableAA(nRF24_PIPE0);
+    // Clear any pending IRQ flags
+    nRF24_ClearIRQFlags();
 
-	// Set operational mode (PTX == transmitter)
-	nRF24_SetOperationalMode(nRF24_MODE_TX);
+    // Wake the transceiver
+    nRF24_SetPowerMode(nRF24_PWR_UP);
 
-	// Clear any pending IRQ flags
-	nRF24_ClearIRQFlags();
+    // Put the transceiver to the RX mode
+    nRF24_CE_H;
 
-	// Wake the transceiver
-	nRF24_SetPowerMode(nRF24_PWR_UP);
 
-	// Some variables
-	uint32_t packets_lost = 0; // global counter of lost packets
-	uint8_t otx;
-	uint8_t otx_plos_cnt; // lost packet count
-	uint8_t otx_arc_cnt; // retransmit count
+    // The main loop
+    while (1) {
+    	//
+    	// Constantly poll the status of the RX FIFO and get a payload if FIFO is not empty
+    	//
+    	// This is far from best solution, but it's ok for testing purposes
+    	// More smart way is to use the IRQ pin :)
+    	//
+    	if (nRF24_GetStatus_RXFIFO() != nRF24_STATUS_RXFIFO_EMPTY) {
+    		// Get a payload from the transceiver
+    		pipe = nRF24_ReadPayload(nRF24_payload, &payload_length);
 
-	// The main loop
-	payload_length = 10;
-	j = 0;
-	while (1) {
-		// Prepare data packet
-		for (i = 0; i < payload_length; i++) {
-			nRF24_payload[i] = j++;
-			if (j > 0x000000FF)
-				j = 0;
-		}
+    		// Clear all pending IRQ flags
+			nRF24_ClearIRQFlags();
 
-		// Print a payload
-		UART_SendStr("PAYLOAD:>");
-		UART_SendBufHex((char*) nRF24_payload, payload_length);
-		UART_SendStr("< ... TX: ");
-
-		// Transmit a packet
-		tx_res = nRF24_TransmitPacket(nRF24_payload, payload_length);
-		otx = nRF24_GetRetransmitCounters();
-		otx_plos_cnt = (otx & nRF24_MASK_PLOS_CNT ) >> 4; // packets lost counter
-		otx_arc_cnt = (otx & nRF24_MASK_ARC_CNT ); // auto retransmissions counter
-		switch (tx_res) {
-		case nRF24_TX_SUCCESS:
-			UART_SendStr("OK");
-			break;
-		case nRF24_TX_TIMEOUT:
-			UART_SendStr("TIMEOUT");
-			break;
-		case nRF24_TX_MAXRT:
-			UART_SendStr("MAX RETRANSMIT");
-			packets_lost += otx_plos_cnt;
-			nRF24_ResetPLOS();
-			break;
-		default:
-			UART_SendStr("ERROR");
-			break;
-		}
-		UART_SendStr("   ARC=");
-		UART_SendInt(otx_arc_cnt);
-		UART_SendStr(" LOST=");
-		UART_SendInt(packets_lost);
-		UART_SendStr("\r\n");
-
-		// Wait ~0.5s
-		Delay_ms(500);
-	}
+			// Print a payload contents to UART
+			UART_SendStr("RCV PIPE#");
+			UART_SendInt(pipe);
+			UART_SendStr(" PAYLOAD:>");
+			UART_SendBufHex((char *)nRF24_payload, payload_length);
+			UART_SendStr("<\r\n");
+			LED_ERROR2_TOGGLE;
+			LED_ERROR_TOGGLE;
+			LED_STATUS_TOGGLE;
+    	}
+    }
 
 }
 
